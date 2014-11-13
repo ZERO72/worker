eea8de6ad177377b0906ef5c4a62b4f5b0ac3f8f
tomromkes <tom@zero72.com>
2014-11-13 13:25:47 +0100

    more files added

--- script/main.js
+++ script/main.js
@@ -1,1 +1,8 @@
 /* Main JS file for WebWorker Test */
+var worker = new Worker('workers/worker.lazyload.js');
+
+	worker.addEventListener('message', function(e){
+		console.log("worker said: ", e.data);
+	}, false);
+
+	worker.postMessage('hello world'); // Start the worker.
--- script/workers/worker.lazyload.js
+++ script/workers/worker.lazyload.js
@@ -1,5 +1,9 @@
 /* Worker File - LazyLoad - source: http://toddmotto.com/echo-js-simple-javascript-image-lazy-loading/ */
 
+self.addEventListener('message', function(e){
+	self.postMessage(e.data);
+}, false);
+
 window.echo = (function (window, document) {
 
   'use strict';
