ef2b2ccf4f12f707ffa792e1287f8081ec4d2ad1
tomromkes <tom@zero72.com>
2014-11-13 13:45:00 +0100

    Check

--- script/main.js
+++ script/main.js
@@ -1,5 +1,5 @@
 /* Main JS file for WebWorker Test */
-<<<<<<< Updated upstream
+
 
 var pageIndex = 1;
 
@@ -24,24 +24,15 @@ var data = {
 
 }
 
-	var worker = new Worker('workers/worker.lazyload.js');
-=======
 var workerAjaxCall = new Worker('script/workers/worker.ajaxcall.js');
->>>>>>> Stashed changes
-
 	
 	workerAjaxCall.addEventListener('message', function(e){
 		console.log("worker said: ", e.data);
 	}, false);
 
-<<<<<<< Updated upstream
-	worker.postMessage('hello world'); // Start the worker.
-
-=======
 	var data = {
 		"url" : "urletje",
  		"msg" : "hello world"
 	};
 
 	workerAjaxCall.postMessage(data); // Start the worker.
->>>>>>> Stashed changes
