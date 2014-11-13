8e2cd1f9311dde75fdbe43580dbfcd303deced38
KostasZERO72 <kostas@zero72.com>
2014-11-13 13:35:36 +0100

    basic files

--- script/main.js
+++ script/main.js
@@ -1,1 +1,29 @@
 /* Main JS file for WebWorker Test */
+
+var pageIndex = 1;
+
+var data = {
+
+	init:function() {
+
+		// get data
+
+
+
+	}
+
+	getNext:function() {
+
+		var url = '../data/page'+pageIndex+'.js';
+
+		jQuery.getJSON( url_json, function( data ) {
+		
+
+		
+		
+		});
+
+
+	}
+
+}
